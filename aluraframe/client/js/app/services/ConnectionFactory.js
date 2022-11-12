d120b56402aa1acbf2b4c946b9ccf4eef5dfdb2c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-11-12 01:46:58 -0300

    Monkey Path

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,27 +1,66 @@
-class ConnectionFactory {
+var ConnectionFactory = (function () {
 
-  constructor() {
-     
+  const stores = ['negociacoes'];
+  const version = 4;
+  const dbName = 'aluraframe';
 
-    throw new error ('Não é possível criar instâncias de ConectionFactory')
-  }
-
-  static getConnection() {
-
-    return Promise ((resolve, reject) => {
-
-      let openRequest = window.indexedDB.open('aluraframe', 4);
-
-      openRequest.onupgradeneeded = e => {
-
-      };
-      openRequest.onsuccess = e => {
-
-      };
-      openRequest.onerror = e => {
-        
+  var connection = null;
+  
+  var close = null;
+  
+  return class ConnectionFactory {
+  
+      constructor() {
+  
+          throw new Error('Não é possível criar instâncias de ConnectionFactory');
+      }
+  
+      static getConnection() {
+  
+          return new Promise((resolve, reject) => {
+  
+              let openRequest = window.indexedDB.open(dbName,version);
+  
+              openRequest.onupgradeneeded = e => {
+  
+                  ConnectionFactory._createStores(e.target.result);
+              };
+  
+              openRequest.onsuccess = e => {    
+  
+                if (!connection) {
+                  connection = e.target.result;
+                  close = connection.close.bind(connection)
+                  connection.close = function() {
+                    throw new Error ('Você não pode fechar diretamente esta conexão');
+                  }
+                }
+                  
+                resolve(connection);
+              };
+  
+              openRequest.onerror = e => {    
+  
+                  console.log(e.target.error);
+  
+                  reject(e.target.error.name);
+              };
+          });
+      }
+      static _createStores(connection) {
+  
+          stores.forEach(store => {
+  
+              if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+              connection.createObjectStore(store, { autoIncrement: true });
+          });
       }
 
-    });
-  }
+      static closeConnection() {
+        if (connection) {
+	        close();
+	        connection = null;
 }
+      }
+  }
+})()
