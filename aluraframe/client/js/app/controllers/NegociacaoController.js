34aae35e7f53250c567746ecd8f519a0ff9c0e4c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-11-14 00:56:50 -0300

    parseInt

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,22 +15,40 @@ class NegociacaoController {
        
         this._mensagem = new Bind(
             new Mensagem(), new MensagemView($('#mensagemView')),
-            'texto');       
+            'texto');    
+            
+        this._ordemAtual = '';
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao => 
+                    this._listaNegociacoes.adiciona(negociacao)))
+               
     }
     
     adiciona(event) {
 
+        event.preventDefault();
         
+        ConnectionFactory
+            .getConnection()
+            .then(connection => {
 
-        try {
-            
-            event.preventDefault();
-            this._listaNegociacoes.adiciona(this._criaNegociacao());
-            this._mensagem.texto = 'Negociação adicionada com sucesso'; 
-            this._limpaFormulario();   
-        } catch (erro) {
-            this._mensagem.texto = erro;
-        }
+                let negociacao = this._criaNegociacao();
+
+                new NegociacaoDao(connection)
+                .adiciona(negociacao)
+                .then(() => {
+                    this._listaNegociacoes.adiciona(negociacao);
+                    this._mensagem.texto = 'Negociação adicionada com sucesso'; 
+                    this._limpaFormulario();
+                })
+            })
+            .catch(erro => this._mensagem.texto = erro);
+
     }
     
     importaNegociacoes() {
@@ -56,8 +74,8 @@ class NegociacaoController {
         
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);    
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));    
     }
     
     _limpaFormulario() {
--- aluraframe/client/js/app/dao/NegociacaoDao.js
+++ aluraframe/client/js/app/dao/NegociacaoDao.js
@@ -2,7 +2,7 @@ class NegociacaoDao {
 
   constructor(connection) {
     this._connection = connection;
-    this.store = 'negociacoes';
+    this._store = 'negociacoes';
   }
 
   adiciona(negociacao) { 
@@ -20,8 +20,46 @@ class NegociacaoDao {
         request.onerror = e => {
 
           console.log(e.target.error);
-          reject('Não foi possível adicionar a negociação');
+          reject('Não foi possível adicionar a negociação')
         }
     });
+  }
+
+  listaTodos() {
+
+    return new Promise((resolve, reject) => {
+
+      let cursor = this._connection
+        .transaction([this._store], 'readwrite')
+        .objectStore(this._store)
+        .openCursor();
+
+        let negociacoes = [];
+
+        cursor.onsuccess = e => {
+
+          let atual = e.target.result;
+
+          if (atual) {
+
+            let dado = atual.value;
+
+            negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor));
+
+            atual.continue();
+
+          } else {
+
+            resolve(negociacoes);
+          }
+
+        };
+
+        cursor.onerror = e => {
+
+          console.log(e.target.error);
+          reject('Não foi possível listar as negociações')
+        };
+    })
   }
 }
